62e77e51273577de9420e84980f61d8e748a6cd4
tamaricashaw <tamaricashaw13@gmail.com>
2026-03-16 18:08:22 -0500

    Listed upload files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, send_from_directory, url_for, flash, session, abort
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
@@ -76,8 +76,43 @@ def login():
         flash('Logged in successfully.', 'success')
 
        
-        return redirect(url_for("upload"))  # The user should be redirected to the upload form instead
+        return redirect(url_for("files"))  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
+
+
+@app.route('/uploads/<filename>', methods=['POST', 'GET'])
+def get_image(filename):
+    
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename) 
+
+
+
+
+
+@app.route('/files')
+@login_required
+def files():
+    
+    images = get_uploaded_images()
+
+    return render_template('files.html', images=images)
+
+#################################
+def get_uploaded_images():
+    images = []
+    
+    upload_folder = app.config['UPLOAD_FOLDER']
+    
+    # Check if the folder exists
+    if os.path.exists(upload_folder):
+        for filename in os.listdir(upload_folder):
+            file_path = os.path.join(upload_folder, filename)
+            if os.path.isfile(file_path):
+                images.append(filename)
+    
+    return images
+
+
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
