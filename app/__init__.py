ddd5b6bf4c9d7f253d994069639821ab533707d3
tamaricashaw <tamaricashaw13@gmail.com>
2026-03-16 13:17:23 -0500

    Defined flask login and created first migration

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 # import flask migrate here
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 # Instantiate Flask-Migrate library here
+migrate = Migrate(app, db)
 
 # Flask-Login login manager
 login_manager = LoginManager()
@@ -16,3 +18,6 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 from app import views
+
+
+
