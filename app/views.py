a70dcf5de10f7c19670260f5cc36e67bb5e04151
tamaricashaw <tamaricashaw13@gmail.com>
2026-03-16 15:08:35 -0500

    Updated login route

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -43,20 +44,31 @@ def login():
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
-        # Get the username and password values from the form.
+    if form.validate_on_submit():
 
-        # Using your model, query database for a user based on the username
+        # Get the username and password values from the form.
+        username= form.username.data
+        password=form.password.data
+
+        # Using your model, query database for a user based on the username 
+        user = db.session.execute(db.select(UserProfile).filter_by(username=username)).scalar()
+        
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
+        if user is not None and check_password_hash(user.password, password):
+            remember_me = False
+        
 
         # Gets user id, load into session
-        login_user(user)
-
+        # login_user(user)
         # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
+        login_user(user, remember=remember_me)
+        flash('Logged in successfully.', 'success')
+
+       
+        return redirect(url_for("upload"))  # The user should be redirected to the upload form instead
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
